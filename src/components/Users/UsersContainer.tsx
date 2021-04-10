33d622232ba9706a0b5fad6dfcbdd3c84ba1e6ab
Pavelkv94 <pavel.kv94@yandex.by>
2021-04-10 20:38:46 +0300

    refactor: move UsersAPI-component in UsersContainer, delete UsersApi.tsx

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,8 +1,9 @@
+import axios from 'axios';
 import { connect } from 'react-redux';
 import { DispatchType, ReduxStateType } from '../../redux/redux-store';
 import { followAC, setCurrentPageAC, setTotalCountAC, setUserAC, unfollowAC, UsersOfSearchType } from '../../redux/usersReducer';
-import { UsersAPiComponent } from './UsersAPiComponent';
-
+import React from 'react';
+import { Users } from './Users';
 type MapStateToPropsType = {
     users: UsersOfSearchType
     pageSize: number
@@ -16,6 +17,48 @@ type MapDispatchToPropsType = {
     setCurrentPage: (currentPage: number) => void
     setTotalCount: (totalUsersCount: number) => void
 }
+
+
+type UsersAPIType = {
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    users: UsersOfSearchType
+    follow: (userId: number) => void,
+    unfollow: (usersId: number) => void
+    setUsers: (users: UsersOfSearchType) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalCount: (totalUsersCount: number) => void
+}
+export class UsersAPiComponent extends React.Component<UsersAPIType>{
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    };
+
+    render() {
+
+        return <Users totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />
+    }
+}
+
 let mapStateToProps = (state: ReduxStateType): MapStateToPropsType => {
     return {
         users: state.usersPage.users,
