886bfb5d1ea053fb33251f8063f2ea2e52417b8f
Pavelkv94 <pavel.kv94@yandex.by>
2021-04-19 19:08:33 +0300

    refactor: DAL refactor baseUrl for api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const baseUrl = "https://social-network.samuraijs.com/api/1.0/"
+
 const instance = axios.create({
     withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
     headers: { "API-KEY": "92cf59a3-3b12-407b-a573-626b86aed9d2" }
 });
 
 
 export const getUsers = (currentPage: any = 1, pageSize: any = 10) => { //!<<<<<<<<<<<<<<<<<<---------------types
-    return instance.get(
-        baseUrl + `users?page=${currentPage}&count=${pageSize}`
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`
     ).then(response => response.data)
 }
