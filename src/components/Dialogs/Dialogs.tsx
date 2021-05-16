0c6dc8467128ec04beaa69ff42b6b0acd3ae30d1
pavel.kv94 <pavel.kv94@yandex.by>
2021-05-16 22:31:23 +0300

    feat: add validation for messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,6 +2,8 @@ import React, { ChangeEvent } from 'react';
 import { Redirect } from 'react-router';
 import { Field, InjectedFormProps, reduxForm } from 'redux-form';
 import { UserMessageType, UserType } from '../../redux/dialogsReducer';
+import { maxLengthCreator, requiredField } from '../../utils/validators/validators';
+import { Textarea } from '../common/FormsControl/FormControl';
 import { DialogItem } from './DialogItems/DialogItem';
 import d from "./Dialogs.module.css"
 import { Message } from './Messages/Message';
@@ -44,12 +46,17 @@ export function Dialogs(props: DialogsType) {
         </div>
     )
 }
-
+const maxLength100 = maxLengthCreator(100);
 const AddMessageForm = (props: InjectedFormProps<NewMessageType>) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field component="textarea" name="newMessageBody" placeholder="Enter your message" />
+                <Field
+                    component={Textarea}
+                    name="newMessageBody"
+                    placeholder="Enter your message"
+                    validate={[requiredField, maxLength100]}
+                />
             </div>
             <div><button>Send</button></div>
         </form>
