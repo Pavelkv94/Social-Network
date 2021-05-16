619ef2ebd9cef0ee500d1bb747bc5feffa2def02
pavel.kv94 <pavel.kv94@yandex.by>
2021-05-16 22:55:30 +0300

    refactor: refactor FormControl

--- src/components/common/FormsControl/FormControl.tsx
+++ src/components/common/FormsControl/FormControl.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import style from './FormControl.module.css';
 
-export const Textarea = ({ input, meta, ...props }: any) => {
+const FormControl = ({ input, meta, child, ...props }: any) => {
     const validationError = meta.touched && meta.error;
     return (
         <div className={style.formControl + " " + (validationError ? style.error : "")}>
             <div>
-                <textarea {...input} {...props} />
+                {props.children}
             </div>
             {validationError && <span>{meta.error}</span>}
         </div>
     )
+}
+
+export const Textarea = (props: any) => {
+    const { input, meta, child, ...restProps } = props;
+    return <FormControl {...props}> <textarea {...input} {...restProps} /></FormControl>
 };
 
-export const Input = ({ input, meta, ...props }: any) => {
-    const validationError = meta.touched && meta.error;
-    return (
-        <div className={style.formControl + " " + (validationError ? style.error : "")}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {validationError && <span>{meta.error}</span>}
-        </div>
-    )
+export const Input = (props: any) => {
+    const { input, meta, child, ...restProps } = props;
+    return <FormControl {...props}> <input {...input} {...restProps} /></FormControl>
 };
+
+//todo то же что и выше но попроще
+// export const Textarea = ({ input, meta, ...props }: any) => {
+//     const validationError = meta.touched && meta.error;
+//     return (
+//         <div className={style.formControl + " " + (validationError ? style.error : "")}>
+//             <div>
+//                 <textarea {...input} {...props} />
+//             </div>
+//             {validationError && <span>{meta.error}</span>}
+//         </div>
+//     )
+// };
+
+// export const Input = ({ input, meta, ...props }: any) => {
+//     const validationError = meta.touched && meta.error;
+//     return (
+//         <div className={style.formControl + " " + (validationError ? style.error : "")}>
+//             <div>
+//                 <input {...input} {...props} />
+//             </div>
+//             {validationError && <span>{meta.error}</span>}
+//         </div>
+//     )
+// };
